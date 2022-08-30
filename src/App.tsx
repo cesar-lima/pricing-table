ce487558757031091936b5620bab466dc7de85a1
cesar-lima <cesarr340@gmail.com>
2022-08-29 22:50:22 -0300

    criando o componente card e seus primeiros estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import Card from "./components/Card"
 import Header from "./components/Header"
 import './styles/global.scss'
 
 export function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <div className="plans">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
   )
 }
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,16 @@
+import  check from '../assets/check.svg';
+import '../styles/card.scss';
+
 export function Card() {
     return(
-        <div>
-            
+        <div className="card">
+            <div className="card-title">
+                Pacote 1
+            </div>
+
+            <div className="card-text">
+                <img src={check} alt="" /> a blablauel
+            </div>
         </div>
     );
 }
